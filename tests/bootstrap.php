8e8191d7f652b2d587c57f3cbe61a55bec6da843
Boone B Gorges <boonebgorges@gmail.com>
2013-06-24 09:44:42 -0400

    Tests bootstrap and Positions save tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,40 @@
 <?php
 
-$GLOBALS['wp_tests_options'] = array(
-    'active_plugins' => array( basename( dirname( dirname( __FILE__ ) ) ) . '/cac-advanced-profiles.php' ),
-);
+if ( ! defined( 'BP_TESTS_DIR' ) ) {
+	define( 'BP_TESTS_DIR', dirname( __FILE__ ) . '/../../buddypress/tests' );
+}
 
-require getenv( 'WP_TESTS_DIR' ) . '/includes/bootstrap.php';
+if ( file_exists( BP_TESTS_DIR . '/bootstrap.php' ) ) :
 
+	require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
+
+	function _bootstrap_cacap() {
+		// Make sure BP is installed and loaded first
+		require BP_TESTS_DIR . '/includes/loader.php';
+
+		// Then load CACAP
+		require dirname( __FILE__ ) . '/../cac-advanced-profiles.php';
+	}
+	tests_add_filter( 'muplugins_loaded', '_bootstrap_cacap' );
+
+	// We need pretty permalinks for some tests
+	function _set_permalinks() {
+		update_option( 'permalink_structure', '/%year%/%monthnum%/%day%/%postname%/' );
+	}
+	tests_add_filter( 'init', '_set_permalinks', 1 );
+
+	// We need pretty permalinks for some tests
+	function _flush() {
+		flush_rewrite_rules();
+	}
+	tests_add_filter( 'init', '_flush', 1000 );
+
+	require getenv( 'WP_TESTS_DIR' ) . '/includes/bootstrap.php';
+
+	// Load the BP test files
+	require BP_TESTS_DIR . '/includes/testcase.php';
+
+	// include our testcase
+//	require( dirname(__FILE__) . '/bp-docs-testcase.php' );
+
+endif;
--- tests/test-sample.php
+++ tests/test-sample.php
@@ -1,10 +1,49 @@
 <?php
 
-class SampleTest extends WP_UnitTestCase {
+class CACAP_Test_Widget_Positions extends BP_UnitTestCase {
 
-	function testSample() {
-		// replace this with some actual testing code
-		$this->assertTrue( true );
+	/**
+	 * @group get_term_for_position
+	 */
+	function test_get_term_for_position_term_exists() {
+		$cwp = new CACAP_Widget_Positions;
+		$term = wp_insert_term( 'foo', 'cacap_position_college' );
+		$term_for_position = $cwp->get_term_for_position( array(
+			'college' => 'foo',
+			'department' => 'bar',
+			'title' => 'baz',
+		), 'college' );
+
+		$this->assertEquals( $term['term_id'], $term_for_position );
+	}
+
+	/**
+	 * @group save_instance_for_user
+	 */
+	function test_save_instance_for_user_no_existing_fields() {
+		$u = $this->create_user();
+		$args = array(
+			'user_id' => $u,
+			'content' => array(
+				array(
+					'college' => 'Foo University',
+					'department' => 'Philosophy',
+					'title' => 'King',
+				),
+				array(
+					'college' => 'Bar University',
+					'department' => 'English',
+					'title' => 'Professor',
+				),
+			),
+		);
+
+		$cacap_widget_positions = new CACAP_Widget_Positions();
+		$cacap_widget_positions->save_instance_for_user( $args );
+
+		$stored_positions = $cacap_widget_positions->get_user_positions( $u );
+
+		$this->assertEquals( $args['content'], $stored_positions );
 	}
 }
 
