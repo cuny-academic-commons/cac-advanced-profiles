9defafc65747f2636ce61cdf65b450f0085c884c
Boone B Gorges <boonebgorges@gmail.com>
2013-06-27 09:13:53 -0400

    Use hardcoded 1 for fullname field

--- templates/cacap/header-top.php
+++ templates/cacap/header-top.php
@@ -6,7 +6,7 @@
 
 <div class="cacap-row cacap-hero-row">
 	<div class="cacap-hero">
-		<h1><?php echo xprofile_get_field_data( 'Full Name', bp_displayed_user_id() ) ?></h1>
+		<h1><?php echo xprofile_get_field_data( 1, bp_displayed_user_id() ) ?></h1>
 		<h4 class="cacap-short-aboutme"><?php echo xprofile_get_field_data( 'Brief Descriptor', bp_displayed_user_id() ) ?></h4>
 		<div class="cacap-long-aboutme">
 			<?php echo wpautop( xprofile_get_field_data( 'About You', bp_displayed_user_id() ) ) ?>
