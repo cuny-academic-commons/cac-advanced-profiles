c77a0e80bcc2c09f3f345cc388fdbdf4bed8a307
Boone B Gorges <boonebgorges@gmail.com>
2013-06-27 08:33:29 -0400

    Implements dynamic header

--- templates/cacap/header.php
+++ templates/cacap/header.php
@@ -3,16 +3,44 @@
 <div class="cacap-row">
 	<dl id="cacap-vitals">
 		<dt>Follow me Online</dt>
-		<dd>Icons</dd>
+		<dd>
+			<?php
+			buddypress()->social_media_profiles->setup_user_sm_fields();
 
-		<dt>Contact</dt>
-		<dd>contact info</dd>
+			foreach ( buddypress()->social_media_profiles->user_sm_fields as $field ) {
+				echo $field['html'];
+			}
+			?>
+		</dd>
 
-		<dt>Website</dt>
-		<dd>foo.bar</dd>
+		<?php
+		$contact_info = array();
 
-		<dt>Blog</dt>
-		<dd>awesome.com</dd>
+		if ( $phone = xprofile_get_field_data( 'Phone', bp_displayed_user_id() ) ) {
+			$contact_info[] = $phone;
+		}
+
+		if ( $email = xprofile_get_field_data( 'Email Address', bp_displayed_user_id() ) ) {
+			$contact_info[] = $email;
+		}
+
+		$contact_info = implode( ' &middot; ', $contact_info );
+		?>
+
+		<?php if ( $contact_info ) : ?>
+			<dt><?php _e( 'Contact', 'cacap' ) ?></dt>
+			<dd><?php echo $contact_info ?></dd>
+		<?php endif ?>
+
+		<?php if ( $website = xprofile_get_field_data( 'Website', bp_displayed_user_id() ) ) : ?>
+			<dt><?php _e( 'Website', 'cacap' ) ?></dt>
+			<dd><a href="<?php echo esc_url( $website ) ?>"><?php echo esc_html( $website ) ?></a></dd>
+		<?php endif ?>
+
+		<?php if ( $blog = xprofile_get_field_data( 'Blog', bp_displayed_user_id() ) ) : ?>
+			<dt><?php _e( 'Blog', 'cacap' ) ?></dt>
+			<dd><a href="<?php echo esc_url( $blog ) ?>"><?php echo esc_html( $blog ) ?></a></dd>
+		<?php endif ?>
 	</dl>
 </div>
 
