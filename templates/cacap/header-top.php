1c07ef2fb2a2f2a1be88ea44187a4af52a5f209e
Boone B Gorges <boonebgorges@gmail.com>
2013-07-03 14:34:48 -0400

    Adds @mention username in header and tooltip

--- templates/cacap/header-top.php
+++ templates/cacap/header-top.php
@@ -6,7 +6,10 @@
 
 <div class="cacap-row cacap-hero-row">
 	<div class="cacap-hero">
-		<h1><a href="<?php echo bp_displayed_user_domain() ?>"><?php echo xprofile_get_field_data( 1, bp_displayed_user_id() ) ?></a></h1>
+		<h1>
+			<a href="<?php echo bp_displayed_user_domain() ?>"><?php echo xprofile_get_field_data( 1, bp_displayed_user_id() ) ?></a>
+			<span class="cacap-at-name">(@<?php bp_displayed_user_username() ?>) <span class="button cacap-at-name-tooltip">?<span class="cacap-at-name-tooltip-text cacap-tooltip"><?php printf( __( 'Mention this user using "@%s" and they will receive notification.', 'cacap' ), bp_get_displayed_user_username() ) ?></span></span>
+		</h1>
 		<h4 class="cacap-short-aboutme"><?php echo xprofile_get_field_data( 'Brief Descriptor', bp_displayed_user_id() ) ?></h4>
 		<div class="cacap-long-aboutme">
 			<?php echo wpautop( xprofile_get_field_data( 'About You', bp_displayed_user_id() ) ) ?>
