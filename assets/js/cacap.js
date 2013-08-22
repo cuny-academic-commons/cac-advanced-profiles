a771a18b57667e61ac3dbf79c55fcdc466618f85
Boone Gorges <boonebgorges@gmail.com>
2013-08-22 11:15:13 -0400

    Only do sticky if the window is tall enough
    This helps with jerkiness

--- assets/js/cacap.js
+++ assets/js/cacap.js
@@ -141,10 +141,13 @@ jQuery(document).ready( function($) {
 		return false;
 	});
 
-	$('.cacap-hero-row').waypoint('sticky', {
-		offset: 40,
-		wrapper: '<div class="cacap-hero-row-sticky" />' 
-	});
+	var window_height = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight;
+	if ( window_height < $(document).height() - 200 ) {
+		$('.cacap-hero-row').waypoint('sticky', {
+			offset: 40,
+			wrapper: '<div class="cacap-hero-row-sticky" />' 
+		});
+	}
 
 	/**
 	 * Positions setup
