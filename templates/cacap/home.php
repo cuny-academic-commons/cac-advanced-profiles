0f7a6ba9167d7c8b21fb7c43267ab0453e9816c9
Boone B Gorges <boonebgorges@gmail.com>
2013-09-25 09:49:51 -0400

    Better IE compat
    We want to force IE not to use compatibility mode (as it's set to
    do at the Graduate Center). So we send the appropriate <meta> tag.
    
    This was not working with the fix introduced in 05a308f7df8865908523b447289290bc360bf2a9
    for two reasons. One, cacap templates are not technically part of
    the bp-nelo theme. Two, there is a problem with the X-UA-Compatible meta
    tag and transitional doctypes (so we switch to html)
    
    See #2798

--- templates/cacap/home.php
+++ templates/cacap/home.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html>
 <?php
 /**
@@ -14,6 +14,7 @@
 <?php /* @todo Call a real header template? Which? */ ?>
 
 <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<title><?php wp_title( '|', true, 'right' ); ?> CUNY Academic Commons</title>
 	<?php wp_head() ?>
 </head>
