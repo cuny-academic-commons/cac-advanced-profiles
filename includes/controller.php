cuny-academic-commons/cac-advanced-profiles

commit de8048b8d228b027dd027de6e659ed4ef3d4fa64

Decode HTML special chars on submitted content.
The encoding can happen during the move to and from the widget cache in JS. The
data we save should not be encoded like this.

includes/controller.php
@@ -254,6 +254,10 @@ class CACAP_Controller {
 					continue;
 				}
 
+				// Content may have converted characters from
+				// JS juggling.
+				$content = htmlspecialchars_decode( $content );
+
 				$key_a = explode( '-', $key );
 				$key_a_last = array_pop( $key_a );
 				if ( 0 === strpos( $key_a_last, 'newwidget' ) ) {

commit 14fb48b7a8bcd61e6e28d3b7f509d797799b2851

Don't decode special characters on non-scalar strings.
Fixes regression introduced into Positions widget in de8048b8d2.

includes/controller.php
@@ -256,7 +256,9 @@ class CACAP_Controller {
 
 				// Content may have converted characters from
 				// JS juggling.
-				$content = htmlspecialchars_decode( $content );
+				if ( is_scalar( $content ) ) {
+					$content = htmlspecialchars_decode( $content );
+				}
 
 				$key_a = explode( '-', $key );
 				$key_a_last = array_pop( $key_a );